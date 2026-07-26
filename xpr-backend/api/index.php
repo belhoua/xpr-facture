47fd8993a9f0dbf4c6d9ef8ebe9e732fadba373b
othmane <othmane.belhouari20@ump.ac.ma>
2026-07-26 21:09:57 +0100

    verification

--- xpr-backend/api/index.php
+++ xpr-backend/api/index.php
@@ -1,3 +1,19 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Charge le fichier autoload de Composer dans xpr-backend/vendor/
+require __DIR__ . '/../vendor/autoload.php';
+
+// Initialise Laravel depuis xpr-backend/bootstrap/
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
